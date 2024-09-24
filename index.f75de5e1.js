5c46f37d1ea8347bc46fc267b3e31fe9c39415c4
Nazar Klimovych <rksvjdbx@gmail.com>
2024-09-24 10:59:39 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,37 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.createElement("div");
+tree.id = "tree";
+document.body.appendChild(tree);
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    element.appendChild(list);
+    for (const entry of Object.entries(data)){
+        const listItem = document.createElement("li");
+        listItem.textContent = entry[0];
+        list.appendChild(listItem);
+        if (Object.keys(entry[1]).length > 0) {
+            const newUl = document.createElement("ul");
+            element.appendChild(newUl);
+            createTree(listItem, entry[1]);
+        }
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
